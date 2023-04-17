fecef3fe2728bf5d7c48ce083765190c183f03d2
Ruan Souza <ruan2014souza17@gmail.com>
2023-04-17 17:04:53 -0300

    Primeiro commit

---
+++ src/main/java/com/ruansouza/APIRestMongo/ApiRestSpringBootMongodbApplication.java
@@ -0,0 +1,13 @@
+package com.ruansouza.APIRestMongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestSpringBootMongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestSpringBootMongodbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ruansouza/APIRestMongo/ApiRestSpringBootMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ruansouza.APIRestMongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestSpringBootMongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
