e2444358e0937bb35779ec199ac0489b8fb37f66
Ruan Souza <ruan2014souza17@gmail.com>
2023-04-19 09:17:29 -0300

    Obtendo usuario por Id e tratamento de excecao

--- src/main/java/com/ruansouza/APIRestMongo/resources/UserResource.java
+++ src/main/java/com/ruansouza/APIRestMongo/resources/UserResource.java
@@ -1,11 +1,11 @@
 package com.ruansouza.APIRestMongo.resources;
 
 import java.util.List;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,13 +19,19 @@ import com.ruansouza.APIRestMongo.services.UserService;
 public class UserResource {
 
 	@Autowired
-	private UserService serv;
+	private UserService service;
 
 	@RequestMapping(method = RequestMethod.GET)
-	public ResponseEntity<List<User>> findAll() {
-		List<User> list = serv.findAll();
+	public ResponseEntity<List<UserDTO>> findAll() {
+		List<User> list = service.findAll();
 		List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
-		return ResponseEntity.ok().body(list);
+		return ResponseEntity.ok().body(listDto);
 	}
 
+	@RequestMapping(value="/{id}", method = RequestMethod.GET)
+	public ResponseEntity<UserDTO> findById(@PathVariable String id) {
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(new UserDTO(obj));
+	}
+	
 }
--- src/main/java/com/ruansouza/APIRestMongo/services/UserService.java
+++ src/main/java/com/ruansouza/APIRestMongo/services/UserService.java
@@ -1,20 +1,28 @@
 package com.ruansouza.APIRestMongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.ruansouza.APIRestMongo.domain.User;
 import com.ruansouza.APIRestMongo.repository.UserRepository;
+import com.ruansouza.APIRestMongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
 
 	@Autowired
 	private UserRepository repo;
-	
-	public List<User> findAll(){
+
+	public List<User> findAll() {
 		return repo.findAll();
+	}
+
+	
+		public User findById(String id) {
+			Optional<User> obj = repo.findById(id);
+			return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 }
