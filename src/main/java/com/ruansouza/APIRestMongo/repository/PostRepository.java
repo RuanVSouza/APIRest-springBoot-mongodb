c23795fe2a074e1dc35108698cee63a474fb52ed
Ruan Souza <ruan2014souza17@gmail.com>
2023-04-25 10:29:56 -0300

    acrescentando comentarios aos posts

--- src/main/java/com/ruansouza/APIRestMongo/repository/PostRepository.java
+++ src/main/java/com/ruansouza/APIRestMongo/repository/PostRepository.java
@@ -4,7 +4,6 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 import com.ruansouza.APIRestMongo.domain.Post;
-import com.ruansouza.APIRestMongo.domain.User;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String>{
